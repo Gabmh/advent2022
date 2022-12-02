db0bbbd83260a85043dc96df0db6afa9bc2bd8d6
Gabmh <mahegabriel@gmail.com>
2022-12-02 10:44:01 +0100

    feat: part 1 answer

--- solution.ts
+++ solution.ts
@@ -2,35 +2,101 @@ const path = require('path')
 const fs = require('fs')
 const JSONStream = require('JSONStream')
 
-const main = () => {
-    const [ path ] = process.argv.slice(2)
-    
-    // wrong params handling
-    if (!path) {
-        console.log(`Please give all params: node [--max_old_space_size=50] solution.ts [PATH]`)
-        return 0
-    } else {
-        console.log('...loading')
-    }
+const main = () => {    
+    // const rockMapping = ['A', 'X']
+    // const paperMapping = ['B', 'Y']
+    // const scissorsMapping = ['C', 'Z']
 
     // reading file by package
-    const raw = fs.readFileSync(path, { encoding: 'utf8' })
+    const raw = fs.readFileSync('input.txt', { encoding: 'utf8' })
     const list = raw.split(/\r?\n/)
 
-    let result = []
-    let count = 0
+    let result = 0
     list.forEach(element => {
-        const item = parseInt(element)
-        if (Number.isInteger(item)) {
-            count += item
-        } else {
-            result.push(count)
-            count = 0
+        let count = 0
+        const [player1, _, player2] = element
+        console.log(`${player1}+${player2}`)
+        switch (player1) {
+            case 'A': {
+                // Rock
+                switch (player2) {
+                    case 'X': {
+                        // Rock
+                        // Draw
+                        count = 1 + 3
+                        break
+                    }
+                    case 'Y': {
+                        // Paper
+                        // Win
+                        count = 2 + 6
+                        break
+                    }
+                    case 'Z': {
+                        // Scissor
+                        // Loose
+                        count = 3 + 0
+                        break
+                    }
+                }
+                break
+            }
+            case 'B': {
+                // Paper
+                switch (player2) {
+                    case 'X': {
+                        // Rock
+                        // Loose
+                        count = 1 + 0
+                        break
+                    }
+                    case 'Y': {
+                        // Paper
+                        // Draw
+                        count = 2 + 3
+                        break
+                    }
+                    case 'Z': {
+                        // Scissor
+                        // Win
+                        count = 3 + 6
+                        break
+                    }
+                }
+                break
+            }
+            case 'C': {
+                //Scissor
+                switch (player2) {
+                    case 'X': {
+                        // Rock
+                        // Win
+                        count = 1 + 6
+                        break
+                    }
+                    case 'Y': {
+                        // Paper
+                        // Loose
+                        count = 2 + 0
+                        break
+                    }
+                    case 'Z': {
+                        // Scissor
+                        // Draw
+                        console.log('test')
+                        count = 3 + 3 
+                        break
+                    }
+                }
+                break
+            }
         }
+        console.log(count)
+        result += count
     })
 
     // printing result
-    console.log('result = ', result.sort((one, two) => (one > two ? -1 : 1)).slice(0, 3).reduce((a, b) => a+b)) 
+    console.log('result = ', result) 
 }
 
 main()
