2b442fb1c6a921e78db46b864b97149aa5a5ecc9
Gabmh <gmahe@gopro.com>
2022-12-03 14:16:19 +0100

    feat: part 1 solution

--- solution.ts
+++ solution.ts
@@ -2,97 +2,34 @@ const path = require('path')
 const fs = require('fs')
 const JSONStream = require('JSONStream')
 
-const main = () => {    
-    // const rockMapping = ['A', 'X']
-    // const paperMapping = ['B', 'Y']
-    // const scissorsMapping = ['C', 'Z']
-
+const main = () => {
     // reading file by package
     const raw = fs.readFileSync('input.txt', { encoding: 'utf8' })
     const list = raw.split(/\r?\n/)
+    list.pop()
 
     let result = 0
     list.forEach(element => {
-        let count = 0
-        const [player1, _, player2] = element
-        console.log(`${player1}+${player2}`)
-        switch (player1) {
-            case 'A': {
-                // Rock
-                switch (player2) {
-                    case 'X': {
-                        // Rock
-                        // Draw
-                        count = 1 + 3
-                        break
-                    }
-                    case 'Y': {
-                        // Paper
-                        // Win
-                        count = 2 + 6
-                        break
-                    }
-                    case 'Z': {
-                        // Scissor
-                        // Loose
-                        count = 3 + 0
-                        break
-                    }
-                }
-                break
-            }
-            case 'B': {
-                // Paper
-                switch (player2) {
-                    case 'X': {
-                        // Rock
-                        // Loose
-                        count = 1 + 0
-                        break
-                    }
-                    case 'Y': {
-                        // Paper
-                        // Draw
-                        count = 2 + 3
-                        break
-                    }
-                    case 'Z': {
-                        // Scissor
-                        // Win
-                        count = 3 + 6
-                        break
-                    }
-                }
-                break
-            }
-            case 'C': {
-                //Scissor
-                switch (player2) {
-                    case 'X': {
-                        // Rock
-                        // Win
-                        count = 1 + 6
-                        break
-                    }
-                    case 'Y': {
-                        // Paper
-                        // Loose
-                        count = 2 + 0
-                        break
-                    }
-                    case 'Z': {
-                        // Scissor
-                        // Draw
-                        console.log('test')
-                        count = 3 + 3 
-                        break
-                    }
-                }
-                break
-            }
-        }
-        console.log(count)
-        result += count
+        // Split a string into two equivalent
+        const middle = element.length / 2 
+        let s1 = element.substring(0, middle);
+        let s2 = element.substring(middle);
+        console.log(`${s1}, ${s2}`)
+        console.log(`${s1.length}, ${s2.length}`)
+
+        // Convert both string into array to find a common character
+        const common = s1.split('').filter(char => s2.split('').includes(char))[0]
+        console.log('common = ', common)
+        
+        // Find the value of this character
+        // Char code of lowercase a is 97
+        // Char code of uppercase A is 65
+        console.log('is lowercase', common === common.toLowerCase())
+        const value = common === common.toLowerCase() ? common.charCodeAt(0) - 96 : common.charCodeAt(0) - 38
+        console.log('value = ', value)
+
+        // Add to the final result
+        result += value
     })
 
     // printing result
