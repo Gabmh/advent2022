433a87cb6c8e28411ecf5b4354d1a58f7b60579c
Gabriel Mahé <mahegabriel@gmail.com>
2022-12-04 10:49:25 +0100

    feat: day 4 solution

--- solution.ts
+++ solution.ts
@@ -6,14 +6,19 @@ const main = () => {
         const list = raw.split(/\r?\n/)
         list.pop()
     
-        // Write your solution here
-        let result = 0
+        let count = 0
         list.forEach(value => {
-            result += value.length
-        }, [])
+            const [pairA, pairB] = value.split(',')
+            const [a1, a2] = pairA.split('-').map(a => parseInt(a))
+            const [b1, b2] = pairB.split('-').map(b => parseInt(b))
+            
+            const result = a1 <= b2 && b1 <= a2 
+            if (result) count++
+            console.log(value, result)
+        })
     
         // Printing result
-        console.log('result = ', result) 
+        console.log('result = ', count) 
 }
 
 main()
