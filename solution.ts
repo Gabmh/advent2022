108e25b222cc4856a94d881cdc3636ff7d1b800e
Gabriel Mahé <mahegabriel@gmail.com>
2022-12-06 06:28:20 +0100

    feat: day 6 part 1 solution

--- solution.ts
+++ solution.ts
@@ -1,19 +1,24 @@
 const fs = require('fs')
 
+function hasRepeats (str) {
+    return /(.).*\1/.test(str);
+}
+
 const main = () => {
         // Reading file by package
         const raw = fs.readFileSync('input.txt', { encoding: 'utf8' })
-        const list = raw.split(/\r?\n/)
-        list.pop()
     
         // Write your solution here
-        let result = 0
-        list.forEach(value => {
-            result += value.length
-        }, [])
+        let index = 4
+        while (index < raw.length) {
+            const test = raw.substring(index - 4, index)
+            if (index < 10) console.log(index, test)
+            if (!hasRepeats(test)) break
+            index++
+        }
     
         // Printing result
-        console.log('result = ', result) 
+        console.log('result = ', index) 
 }
 
 main()
